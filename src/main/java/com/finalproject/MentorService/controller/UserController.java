14b3f62bad0058b63b78be1a50ecec31c0f84cd5
Keerthana <keerthanaparsa23@gmail.com>
2024-11-28 23:42:21 -0800

    Adds changes to send/receive messages in real time

--- src/main/java/com/finalproject/MentorService/controller/UserController.java
+++ src/main/java/com/finalproject/MentorService/controller/UserController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,10 +19,13 @@ import java.util.Optional;
 public class UserController {
 
     private final UserService userService;
+    private final SimpMessagingTemplate messagingTemplate;
+
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, SimpMessagingTemplate messagingTemplate) {
         this.userService = userService;
+        this.messagingTemplate = messagingTemplate;
     }
 
     @PostMapping("/users")
@@ -211,6 +215,7 @@ public class UserController {
         try {
             Message createdMessage = userService.createMessage(message);
             if (createdMessage != null) {
+                messagingTemplate.convertAndSend("/topic/return", createdMessage);
                 return ResponseEntity.status(HttpStatus.CREATED).body(createdMessage);
             } else {
                 return ResponseEntity.badRequest().build();
